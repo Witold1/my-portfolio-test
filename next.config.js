599d4c82a0eb2b5c3ba05e98fef171a0bdf155ae
Your Name <you@example.com>
2025-06-30 14:12:07 -0400

    Gallerty update 4.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/my-portfolio-test',
+  assetPrefix: '/my-portfolio-test/',
   trailingSlash: true,
   images: {
     unoptimized: true
